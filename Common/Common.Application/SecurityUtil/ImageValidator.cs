c045cd080beb07911de21e05076b3df41a48c2fe
soheil aghaei <soheil.sa7813@gmail.com>
2022-08-27 20:56:41 +0430

    Common Application & Application Foldering

---
+++ Common/Common.Application/SecurityUtil/ImageValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Http;
+
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
+
+namespace Common.Application.SecurityUtil;
+
+public static class ImageValidator
+{
+    public static bool IsImage(this IFormFile file)
+    {
+        try
+        {
+            var img = Image.FromStream(file.OpenReadStream());
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
---
+++ Common/Common.Application/Validation/CustomValidation/IFormFile/FileImageAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
+
+
+namespace Common.Application.Validation.CustomValidation.IFormFile;
+
+public class FileImageAttribute : ValidationAttribute, IClientModelValidator
+{
+    public override bool IsValid(object? value)
+    {
+        var fileInput = value as Microsoft.AspNetCore.Http.IFormFile;
+        if (fileInput == null)
+            return true;
+
+        return fileInput.IsImage();
+    }
+
+    public void AddValidation(ClientModelValidationContext context)
+    {
+        if (!context.Attributes.ContainsKey("data-val"))
+            context.Attributes.Add("data-val", "true");
+        context.Attributes.Add("accept", "image/*");
+        context.Attributes.Add("data-val-fileImage", ErrorMessage);
+    }
+}
+static class Validation
+{
+    public static bool IsImage(this Microsoft.AspNetCore.Http.IFormFile file)
+    {
+        try
+        {
+            var img = Image.FromStream(file.OpenReadStream());
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
