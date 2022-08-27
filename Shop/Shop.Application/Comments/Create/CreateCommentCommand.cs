55bdeee7be43f70101fa91b817318caa1e0c301d
soheil aghaei <soheil.sa7813@gmail.com>
2022-08-28 01:40:18 +0430

    Comment UseCase

---
+++ Shop/Shop.Application/Comments/Create/CreateCommentCommand.cs
@@ -0,0 +1,5 @@
+﻿using Common.Application;
+
+namespace Shop.Application.Comments.Create;
+
+public record CreateCommentCommand(long UserId, long ProductId, string Text) : IBaseCommand;
---
+++ Shop/Shop.Application/Comments/Edit/EditCommentCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Common.Application;
+using FluentValidation;
+
+namespace Shop.Application.Comments.Edit;
+
+public class EditCommentCommandValidator:AbstractValidator<EditCommentCommand>
+{
+    public EditCommentCommandValidator()
+    {
+        RuleFor(r => r.Text)
+            .NotNull().MinimumLength(5).WithMessage(ValidationMessages.minLength("متن نظر", 5));
+    }
+}
